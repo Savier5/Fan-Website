c77537fd959ce4561ee914464be69f8d1c9f1485
Savier Osman <55478673+Savier5@users.noreply.github.com>
2022-10-17 18:50:59 -0700

    Add files via upload

---
+++ qtyOrdered3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+test();
+setInterval("test()", 500);
+
+function test() {
+   var orderTotal = 0;
+   var price = 400;
+
+   var taxes = document.getElementById("qty").value * 0.0775;
+   var orderTotal = price * taxes + price * document.getElementById("qty").value;
+
+   document.getElementById("orderTotal").innerHTML = orderTotal.toLocaleString("en-US", { style: "currency", currency: "USD" });
+}
